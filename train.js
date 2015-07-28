33569c2795151dcaec2a7c38d38968fefa38b503
Andrew Deniszczyc <andy.deniszczyc@gmail.com>
2015-07-28 11:34:11 +0100

    Added map

--- train.js
+++ train.js
@@ -1,5 +1,9 @@
 if (Meteor.isClient) {
 
+	Template.home.created = function() {
+	  Meteor.Loader.loadJs("https://maps.googleapis.com/maps/api/js?key=AIzaSyBVrZhSoNT6AExUh70_sHNSUzenFi8wHKM&libraries=geometry,places&callback=init");
+	};
+
 }
 
 if (Meteor.isServer) {
