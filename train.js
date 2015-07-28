0258eb1977609335632ac4b526cce09b3529cce2
InTheLittleWood <dragon@dugar.co.uk>
2015-07-28 11:18:11 +0100

    Added new pages- added iron, created multiple templates all sorts. Will get coffee soon ^-^

--- train.js
+++ train.js
@@ -1,7 +1,16 @@
 if (Meteor.isClient) {
+  Template.home.events({
+    
+  });
+  Template.home.rendered = function() {
 
+  }
 }
-
+Router.route('/', function () {
+  this.render('home');
+});
+  // code to create new url
+  Router.route('/items');
 if (Meteor.isServer) {
   Meteor.startup(function () {
     // code to run on server at startup
