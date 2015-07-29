93642ec59fef8bb61c7e381af6d66931af43d935
InTheLittleWood <dragon@dugar.co.uk>
2015-07-29 11:01:06 +0100

    Added some things.

--- train.js
+++ train.js
@@ -1,12 +1,14 @@
 if (Meteor.isClient) {
-  Template.home.events({
-
+  Template.home.events ({
   });
 
   Template.home.created = function() {
     Meteor.Loader.loadJs("https://maps.googleapis.com/maps/api/js?key=AIzaSyBVrZhSoNT6AExUh70_sHNSUzenFi8wHKM&libraries=geometry,places&callback=init");
   };
-
+  Template.home.isFirstRun = function(){
+     // because the Session variable will most probably be undefined the first time
+     return !Session.get("hasRun");
+  }
   Template.home.rendered = function() {
 
     var template = this;
@@ -19,9 +21,9 @@ if (Meteor.isClient) {
       'side': "right"
     });
     // Toggle button
-  document.querySelector('.toggle-button').addEventListener('click', function() {
-    slideoutInstance.toggle();
-  });
+    document.querySelector('.toggle-button').addEventListener('click', function() {
+      slideoutInstance.toggle();
+    });
   }
 
 }
